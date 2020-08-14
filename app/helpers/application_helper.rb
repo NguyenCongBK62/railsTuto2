58fa7a35ece7bef153f8223f82eac2fda832508a
NguyenCongBK62 <conghoa1cnh@gmail.com>
2020-08-14 09:38:10 +0700

    static-pages commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+      def javascript_exists?(controller_name)
+        File.exist?(Rails.root.join("app", "frontend", "packs", "#{controller_name}.js"))
+      end
+    
+      def stylesheet_exists?(controller_name)
+        File.exist?(Rails.root.join("app", "assets", "stylesheets", "#{controller_name}.scss"))
+      end
 end
